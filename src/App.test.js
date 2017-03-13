723bd194acf7742d69915f8e433068970a806a13
dalcib <dalcib@gmail.com>
2017-03-12 21:19:01 -0300

    Removing all files in .gitignore

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-//import * as React from 'react';
-//import ReactDOM from 'react-dom';
-//import App from './App';
-it('renders without crashing', () => {
-    //const div = document.createElement('div');
-    //ReactDOM.render(<App />, div);
-    expect(26).toBe(2 + 4);
-});
-//# sourceMappingURL=App.test.js.map
