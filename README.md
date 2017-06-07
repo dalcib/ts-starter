61b094c2bdca38742fc8cec3779eff154abcb15c
dalcib <dalcib@gmail.com>
2017-06-07 08:24:34 -0400

    Update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # README
+
+SET BABEL_ENV=productiona
+
+"preset": "jest-expo",
+"globals": {
+        "__DEV__": true
+    },
--- app/App.test.tsx
+++ app/App.test.tsx
@@ -1,9 +1,14 @@
-//import * as React from 'react';
+import * as React from 'react';
 //import ReactDOM from 'react-dom';
-//import App from './App';
+import App from './App';
 
 it('renders without crashing', () => {
   //const div = document.createElement('div');
   //ReactDOM.render(<App />, div);
   expect(7).toBe(2 + 4)
 })
+
+it('snapshot App', () => {
+  expect(App).toMatchSnapshot()
+}
+)
