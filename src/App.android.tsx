76a184f2d5d147f7669daaa7c20e8528b998fc85
dalcib <dalcib@gmail.com>
2017-06-18 20:06:13 -0400

    update

--- src/App.android.tsx
+++ src/App.android.tsx
@@ -8,7 +8,7 @@ export default class App extends React.Component<any, any> {
         <Text>Open up App.js to start working on your app !</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>
-          Shake your phone to open the developer menu.
+          Shake your phone to open the developer menu. llgggg
         </Text>
       </View>
     )
